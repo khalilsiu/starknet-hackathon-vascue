622ed633f87c21b381f07a2193ca0b6a56d67f25
Calvin Tang <tangkiwan@gmail.com>
2022-07-13 19:51:50 +0800

    feat: add test cases for register doctor and nurse

--- starknet/tests/test_contract.py
+++ starknet/tests/test_contract.py
@@ -33,22 +33,30 @@ async def contract_factory() -> Tuple[Starknet, Account, Account, StarknetContra
             "contracts/Account.cairo", constructor_calldata=[some_signer.public_key]
         ),
     )
-    some_other_signer = MockSigner(private_key=123456789)
+    some_other_signer_for_doctor = MockSigner(private_key=123456789)
     doctor_account = Account(
-        signer=some_other_signer,
+        signer=some_other_signer_for_doctor,
         contract=await starknet.deploy(
             "contracts/Account.cairo",
-            constructor_calldata=[some_other_signer.public_key],
+            constructor_calldata=[some_other_signer_for_doctor.public_key],
+        ),
+    )
+    some_other_signer_for_nurse = MockSigner(private_key=123456789)
+    nurse_account = Account(
+        signer=some_other_signer_for_nurse,
+        contract=await starknet.deploy(
+            "contracts/Account.cairo",
+            constructor_calldata=[some_other_signer_for_nurse.public_key],
         ),
     )
     contract = await starknet.deploy("contracts/contract.cairo", constructor_calldata=[owner_account.contract.contract_address])
-    return starknet, owner_account, doctor_account, contract
+    return starknet, owner_account, doctor_account, nurse_account, contract
 
 
 @pytest.mark.asyncio
-async def test_register_doctor(contract_factory):
-    """Should register doctor to a given owner address"""
-    _, owner_account, doctor_account, contract = contract_factory
+async def test_register_doctor_by_owner(contract_factory):
+    """Owner should be able to register doctor address to a given id"""
+    _, owner_account, doctor_account, _, contract = contract_factory
 
     await owner_account.signer.send_transaction(
         account=owner_account.contract,
@@ -60,6 +68,56 @@ async def test_register_doctor(contract_factory):
     # Check the owner is registered
     observed_doctor = await contract.get_doctor_id(address=doctor_account.contract.contract_address).call()
     assert observed_doctor.result == (some_doctor_id,)
+
+@pytest.mark.asyncio
+async def test_register_doctor_by_nurse(contract_factory):
+    """Non-Owner should not be able to register doctor address to a given id"""
+    _, _, doctor_account, nurse_account, contract = contract_factory
+
+    with pytest.raises(StarkException):
+        await nurse_account.signer.send_transaction(
+            account=nurse_account.contract,
+            to=contract.contract_address,
+            selector_name="register_doctor",
+            calldata=[some_doctor_id, doctor_account.contract.contract_address],
+        )
+
+    # Check the owner is registered
+    observed_doctor = await contract.get_doctor_id(address=doctor_account.contract.contract_address).call()
+    assert observed_doctor.result == (some_doctor_id,)
+
+@pytest.mark.asyncio
+async def test_register_nurse_by_owner(contract_factory):
+    """Owner should be able to register nurse address to a given id"""
+    _, owner_account, _, nurse_account, contract = contract_factory
+
+    await owner_account.signer.send_transaction(
+        account=owner_account.contract,
+        to=contract.contract_address,
+        selector_name="register_nurse",
+        calldata=[some_nurse_id, nurse_account.contract.contract_address],
+    )
+
+    # Check the owner is registered
+    observed_nurse = await contract.get_nurse_id(address=nurse_account.contract.contract_address).call()
+    assert observed_nurse.result == (some_nurse_id,)
+
+@pytest.mark.asyncio
+async def test_register_nurse_by_doctor(contract_factory):
+    """Non-Owner should not be able to register nurse address to a given id"""
+    _, _, doctor_account, nurse_account, contract = contract_factory
+
+    with pytest.raises(StarkException):
+        await doctor_account.signer.send_transaction(
+            account=doctor_account.contract,
+            to=contract.contract_address,
+            selector_name="register_nurse",
+            calldata=[some_nurse_id, nurse_account.contract.contract_address],
+        )
+
+    # Check the owner is registered
+    observed_nurse = await contract.get_nurse_id(address=nurse_account.contract.contract_address).call()
+    assert observed_nurse.result == (some_nurse_id,)
 
 
 # @pytest.mark.asyncio
