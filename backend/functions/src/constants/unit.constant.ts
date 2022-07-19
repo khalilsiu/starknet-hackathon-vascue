a90df012cda869100e950a7dee74b439fc74908f
Calvin Tang <tangkiwan@gmail.com>
2022-07-19 18:13:50 +0800

    chore: amend double qwuotes

--- backend/functions/src/constants/unit.constant.ts
+++ backend/functions/src/constants/unit.constant.ts
@@ -1,8 +1,8 @@
 export enum Unit {
   PILL = "pills",
   ML = "ml",
-  MG = 'mg',
-  TABLETS = 'tablets'
+  MG = "mg",
+  TABLETS = "tablets"
 }
 
 export const UNITS = [Unit.PILL, Unit.ML, Unit.MG, Unit.TABLETS];
