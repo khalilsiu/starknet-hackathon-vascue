bdf5f534fb717327d193d965aed0f6f03d3db848
Calvin Tang <tangkiwan@gmail.com>
2022-07-19 17:19:41 +0800

    fix: update variable name for api url in env var

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,3 +1,3 @@
 export const config = {
-  apiUrl: process.env.VASCUE_API_URL || "http://localhost:5001/vascue-starknet/europe-west3/vascueStarknetApi/api"
+  apiUrl: process.env.NEXT_PUBLIC_VASCUE_API_URL || "http://localhost:5001/vascue-starknet/europe-west3/vascueStarknetApi/api"
 }
